51d2018542cbba5b769943dc9690ee9a226e8ba7
Jonatan Missora <jonatanjmissora@gmail.com>
2025-07-25 12:45:00 -0300

    arreglado el line grid, y el ampliar

--- src/components/Dashboard/Content/dashboard-file-info.tsx
+++ src/components/Dashboard/Content/dashboard-file-info.tsx
@@ -1,5 +1,15 @@
+import {
+	Dialog,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog"
 import { CloudinaryAsset } from "@/lib/types"
 import { setFileDate, setFileSize } from "@/lib/utils"
+import { Expand } from "lucide-react"
+import Image from "next/image"
 
 export const DashboardFileInfo = ({
 	asset,
@@ -8,25 +18,63 @@ export const DashboardFileInfo = ({
 	asset: CloudinaryAsset
 	view: "grid" | "list"
 }) => {
-
-	if(view === "grid") 
+	if (view === "grid")
 		return (
-			<div className="group left-0 absolute bottom-0 right-0 text-black group-hover:opacity-100 opacity-0 bg-[var(--foreground)]/50 p-1 sm:text-xs 2xl:text-sm">
-				<div className="flex items-center justify-center">
-					<span className="w-3/4 truncate text-center">{asset.display_name}</span>
+			<>
+				<Ampliar asset={asset} />
+				<div className="group left-0 absolute bottom-0 right-0 text-[var(--foreground)]/75 group-hover:opacity-100 opacity-0 bg-[var(--background)]/50 p-1 sm:text-xs 2xl:text-sm">
+					<div className="flex items-center justify-center">
+						<span className="w-3/4 truncate text-center">
+							{asset.display_name}
+						</span>
+					</div>
+					<div className={`flex items-center justify-between`}>
+						<span>{setFileDate(asset.created_at)}</span>
+						<span>{setFileSize(asset.bytes)}</span>
+					</div>
 				</div>
-				<div className={`flex items-center justify-between`}>
+			</>
+		)
+	else
+		return (
+			<>
+				<Ampliar asset={asset} />
+				<div className="group flex-1 flex flex-col justify-center gap-2 p-1 sm:text-sm 2xl:text-base">
+					<span className="w-1/2 truncate">{asset.display_name}</span>
 					<span>{setFileDate(asset.created_at)}</span>
 					<span>{setFileSize(asset.bytes)}</span>
 				</div>
-			</div>
+			</>
 		)
-	else 
-		return (
-			<div className="group flex-1 flex flex-col justify-center gap-2 p-1 sm:text-sm 2xl:text-base">
-				<span className="w-1/2 truncate">{asset.display_name}</span>
-				<span>{setFileDate(asset.created_at)}</span>
-				<span>{setFileSize(asset.bytes)}</span>
-			</div>
-		)
+}
+
+const Ampliar = ({ asset }: { asset: CloudinaryAsset }) => {
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Expand className="size-8 group left-0 absolute top-0 text-[var(--foreground)]/75 group-hover:opacity-100 opacity-0 bg-[var(--background)]/50 p-2" />
+			</DialogTrigger>
+			<DialogContent className="max-w-6xl">
+				<DialogHeader>
+					<DialogTitle></DialogTitle>
+				</DialogHeader>
+				<div className="relative w-full h-[80dvh]">
+					<Image
+						src={asset.secure_url}
+						alt={asset.public_id}
+						fill
+						quality={100}
+						priority
+						objectFit="contain"
+						className=""
+					/>
+				</div>
+				<DialogFooter className="w-full flex justify-around items-center text-[var(--foreground)]/75">
+					<span>{setFileDate(asset.created_at)}</span>
+					<span>{asset.display_name}</span>
+					<span>{setFileSize(asset.bytes)}</span>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	)
 }
--- src/components/Dashboard/Content/files-list-grid.tsx
+++ src/components/Dashboard/Content/files-list-grid.tsx
@@ -3,27 +3,34 @@ import Image from "next/image"
 import { DashboardFileMenu } from "./dashboard-file-menu"
 import { DashboardFileInfo } from "./dashboard-file-info"
 
-export const  FilesListGrid = ({sortedFolders}: {sortedFolders: CloudinaryAsset[]}) => {
-
-    return (
-        <article className={`w-full h-full columns-[200px] gap-2`}>
-            {sortedFolders?.map(asset => (
-                <div key={asset.public_id} className={`w-full h-auto relative group border-4 border-transparent hover:border-orange-500/50`}>
-                    <Image
-                        src={asset.secure_url}
-                        alt={asset.public_id}
-                        width={300}
-                        height={300}
-                        quality={100}
-                        priority
-                        objectFit="cover"
-                        className={`w-full`}
-                    />
-                    <DashboardFileMenu asset={asset} view="grid"/>
-                    <DashboardFileInfo asset={asset} view="grid"/>
-                </div>
-            ))}
-        </article>
-
-    )
+export const FilesListGrid = ({
+	sortedFolders,
+}: {
+	sortedFolders: CloudinaryAsset[]
+}) => {
+	return (
+		<article
+			className={`w-full h-full columns-[1fr] sm:columns-[200px] 2xl:columns-[300px] gap-2`}
+		>
+			{sortedFolders?.map(asset => (
+				<div
+					key={asset.public_id}
+					className={`w-full h-auto relative group border-4 border-transparent hover:border-orange-500/50`}
+				>
+					<Image
+						src={asset.secure_url}
+						alt={asset.public_id}
+						width={300}
+						height={300}
+						quality={100}
+						priority
+						objectFit="cover"
+						className={`w-full`}
+					/>
+					<DashboardFileMenu asset={asset} view="grid" />
+					<DashboardFileInfo asset={asset} view="grid" />
+				</div>
+			))}
+		</article>
+	)
 }
