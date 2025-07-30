84785df23ab5f3f38215d163521f4d39a151e22a
Jonatan Missora <jonatanjmissora@gmail.com>
2025-07-30 17:01:39 -0300

    new

--- src/components/layout/upload-form.tsx
+++ src/components/layout/upload-form.tsx
@@ -20,3 +20,33 @@ export default function UploadForm() {
 		</form>
 	)
 }
+
+// function Input() {
+//     const [image, setImage] = useState("");
+
+//     const uploadImage = (files) => {
+//       const formData = new FormData();
+
+//       formData.append("file", files[0]);
+//       formData.append("upload_preset", "<your upload preset>");
+//       fetch(
+//         "https://api.cloudinary.com/v1_1/<your cloud name>/image/upload",
+//         {
+//           method: "POST",
+//           body: formData,
+//         }
+//       )
+//         .then((response) => response.json())
+//         .then((data) => {
+//           setImage(data.secure_url);
+//         });
+//     };
+//     return <div>
+//           <input type="file" onChange={(e) => uploadImage(e.target.files)} />
+//           <img
+//               src={image}
+//               alt="uploaded image"
+//           />
+//     </div>;
+//   }
+//   export default Input;
