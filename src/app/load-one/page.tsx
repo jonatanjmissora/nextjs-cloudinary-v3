1bec97d5be994e98e643c61bd56910df5babd2db
Jonatan Missora <jonatanjmissora@gmail.com>
2025-08-11 16:40:03 -0300

    continuamos testeando imagenes

--- src/app/load-one/page.tsx
+++ src/app/load-one/page.tsx
@@ -1,90 +1,79 @@
 "use client"
-import { useGetAssets } from "@/lib/use-get-assets"
 import Image from "next/image"
 import { CldImage } from "next-cloudinary"
+import pic001 from "../assets/pic001.jpg"
+import pic002 from "../assets/pic002.jpg"
 
-export default function page() {
-	const { isFetching, assets, error } = useGetAssets()
-
-	if (error) return <p>Hay un error</p>
-	if (isFetching) return <p>Cargando...</p>
-
+export default function LoadOnePage() {
 	const assetsData = [
-		{ src: assets[0].secure_url, alt: assets[0].public_id },
-		{ src: assets[1].secure_url, alt: assets[1].public_id },
-		{ src: assets[2].secure_url, alt: assets[2].public_id },
-		{ src: assets[3].secure_url, alt: assets[3].public_id },
+		{
+			secure_url:
+				"https://res.cloudinary.com/dfvzvdpfa/image/upload/e_grayscale/zdu9flifeccyo3nyto9e.jpg",
+			public_id: "zdu9flifeccyo3nyto9e",
+		},
+		{
+			secure_url:
+				"https://res.cloudinary.com/dfvzvdpfa/image/upload/e_blur:10/ac0e2oqzhxrc4cb83jdl.jpg",
+			public_id: "ac0e2oqzhxrc4cb83jdl",
+		},
 	]
 
 	return (
-		<div className="w-full h-screen flex items-center justify-center bg-blue-900">
-			<div className="w-full flex flex-wrap border h-full">
-				<h2>Next Image</h2>
+		<div className="w-full min-h-screen flex items-center justify-center bg-blue-900 p-4">
+			<div className="w-full flex gap-1">
+				<div className="flex-1 flex flex-col gap-2">
+					<h2>Next.js Image with Cloudinary</h2>
+					{assetsData.map(asset => (
+						<div
+							key={`next-${asset.public_id}`}
+							className="relative w-full aspect-[3/2]"
+						>
+							<Image
+								src={asset.secure_url}
+								alt={asset.public_id}
+								fill
+								priority
+							/>
+						</div>
+					))}
+				</div>
 
-				<Image
-					src={assetsData[0].src}
-					alt={assetsData[0].alt}
-					width={300}
-					height={300}
-					priority
-					quality={100}
-					sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-				/>
-				<Image
-					src={assetsData[1].src}
-					alt={assetsData[1].alt}
-					width={300}
-					height={300}
-					priority
-					quality={100}
-					sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-				/>
-				<Image
-					src={assetsData[2].src}
-					alt={assetsData[2].alt}
-					width={300}
-					height={300}
-					priority
-					quality={100}
-				/>
-				<Image
-					src={assetsData[3].src}
-					alt={assetsData[3].alt}
-					width={300}
-					height={300}
-					priority
-					quality={100}
-				/>
-			</div>
-			<div className="w-1/2 border h-full">
-				<h2>CLDImage</h2>
+				{/* <div className="flex-1 flex flex-col gap-2">
+					<h2>CldImage</h2>
+					{assetsData.map(asset => (
+						<div
+							key={`cld-${asset.public_id}`}
+							className="relative w-full aspect-[3/2]"
+						>
+							<CldImage
+								src={asset.public_id}
+								alt={asset.public_id}
+								fill
+								priority
+							/>
+						</div>
+					))}
+				</div> */}
 
-				<CldImage
-					src="samples/breakfast"
-					alt={assetsData[0].alt}
-					width={300}
-					height={300}
-					priority
-					quality={100}
-				/>
+				<div className="flex-1 flex flex-col gap-2">
+					<h2>Next.js Image local</h2>
+					<div className="relative w-full aspect-[3/2]">
+						<Image src="/001.jpg" alt="001.jpg" fill priority />
+					</div>
+					<div className="relative w-full aspect-[3/2]">
+						<Image src="/002.jpg" alt="002.jpg" fill priority />
+					</div>
+				</div>
 
-				<CldImage
-					src={assetsData[1].alt}
-					alt={assetsData[1].alt}
-					width={300}
-					height={300}
-					priority
-					quality={100}
-				/>
-
-				<CldImage
-					src={assetsData[2].alt}
-					alt={assetsData[2].alt}
-					width={300}
-					height={300}
-					priority
-					quality={100}
-				/>
+				<div className="flex-1 flex flex-col gap-2">
+					<h2>Next.js Image static</h2>
+					<div className="relative w-full aspect-[3/2]">
+						<Image src={pic001} alt="001.jpg" priority placeholder="blur" />
+					</div>
+					<div className="relative w-full aspect-[3/2]">
+						<Image src={pic002} alt="002.jpg" priority placeholder="blur" />
+					</div>
+				</div>
 			</div>
 		</div>
 	)
